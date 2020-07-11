1fe1f3e94086e0065dcf8739e603c38d6312178a
Chindalath Traymany <chindalatht@gmail.com>
2020-07-11 17:25:59 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,23 @@
-require 'pry'
-
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+  self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentences_array = self.split(/[.!?]/)
+    sentences_array.delete_if {|i| i.empty?}
+    sentences_array.count
   end
 end
+
+
+
