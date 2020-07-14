d8c2474d32424105e8ee6fdbce11f1317cea1da1
Jonathan Chasse <jonathan.r.chasse@gmail.com>
2020-07-14 17:25:18 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    # self.split("!", "." , "!").delete("").count
+    # self.split("!" || "." || "!").count
+    #then remove empty elements
+    new_array = self.split("!" || "." || "!").delete_if(&:blank?)
+    return new_array.count
+
+
 
   end
 end
