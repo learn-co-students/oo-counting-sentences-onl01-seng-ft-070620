38a2b862bdcfb2fcde1a654da59a4b4e1dda0fc0
Andre Cunningham <andrecunningham62@gmail.com>
2020-07-15 02:12:53 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,39 @@
 require 'pry'
 
 class String
+attr_accessor :name
 
-  def sentence?
-    
-  end
+def sentence?
+ self.end_with?(".")
+end
+  
+     
+ 
+  # def initialize(name)
+  #   @name = name
+  # end
+ 
+    # def say hi
+    #   "HI!"
+    # end
+   
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
 
-  end
+periods = self.scan(/\./)
+excla = self.scan(/\!/)
+questions = self.scan(/\?/)
+array = self.split (/\.|\?|\!/)
+array.delete_if {|sent| sent.size <2}
+  return array.count
 end
+end
+
