aabc9357cc3131e00c3e4b6293c3b501c696652d
Carter Parrett <cparrett94@hotmail.com>
2020-07-15 07:56:34 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+ array_sentence = self.split(/[.?!]/)
+ array_sentence.delete_if{|element| element.length == 0}
+ array_sentence.count
   end
 end
