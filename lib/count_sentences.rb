ab149dcf436ae31c5bb31d1aa486d87c6a62265e
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-15 04:00:52 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+   sentence_num = self.split(/[.?!]/)
+   sentence_num.delete_if {|x| x.length == 0}
+   sentence_num.length
   end
+  
 end
