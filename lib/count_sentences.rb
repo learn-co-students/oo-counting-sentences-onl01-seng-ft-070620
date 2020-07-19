3a5d68d44e14e0c1399339719f7ce4063a343404
LambCake21420 <caylaluiz@gmail.com>
2020-07-19 15:17:01 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
+
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    ##binding.pry
+ self.split(/\.|\?|\!/).delete_if {|sentence| sentence.length < 2}.count
+ 
+end
+  
 end
